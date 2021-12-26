eadf668fdc412e15a7ec26401956e0c0959b8269
Igor <igor1309@gmail.com>
2021-12-26 03:05:39 +0300

    add SplashPublishPlugin

--- Sources/igor1309.dev/main.swift
+++ Sources/igor1309.dev/main.swift
@@ -1,20 +1,21 @@
 import Foundation
-import Publish
 import Plot
+import Publish
+import SplashPublishPlugin
 
 // This type acts as the configuration for your website.
 struct Igor1309Dev: Website {
     enum SectionID: String, WebsiteSectionID {
         // Add the sections that you want your website to contain here:
         case portfolio
-        case posts = "blog"
+        case posts// = "blog"
         case about
     }
-
+    
     struct ItemMetadata: WebsiteItemMetadata {
         // Add any site-specific metadata that you want to use here.
     }
-
+    
     // Update these properties to configure your website:
     var url = URL(string: "https://igor1309.github.com")!
     var name = "Igor Malyarov"
@@ -24,4 +25,24 @@ struct Igor1309Dev: Website {
 }
 
 // This will generate your website using selected theme:
-try Igor1309Dev().publish(withTheme: .igor1309)
+try Igor1309Dev().publish(
+    using: [
+        .installPlugin(
+            .splash(withClassPrefix: "classPrefix")
+        ),
+        .copyResources(),
+        .addMarkdownFiles(),
+        .sortItems(in: .posts, by: \.date, order: .descending),
+        .generateHTML(withTheme: .igor1309),
+        .generateRSSFeed(including: [.posts]),
+        .generateSiteMap()
+    ]
+)
+
+// Publishing Igor Malyarov (6 steps)
+// [1/6] Copy 'Resources' files
+// [2/6] Add Markdown files from 'Content' folder
+// [3/6] Sort items
+// [4/6] Generate HTML
+// [5/6] Generate RSS feed
+// [6/6] Generate site map
