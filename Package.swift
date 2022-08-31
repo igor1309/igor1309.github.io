344bb7568306d3a913a0a7939bebee1a834a39a1
Igor Malyarov <igor1309@gmail.com>
2022-09-01 01:43:56 +0300

    WIP: add basic test

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "igor1309.dev",
+    name: "igor1309dev",
     platforms: [
         .macOS(.v12)
     ],
     products: [
         .executable(
-            name: "igor1309.dev",
-            targets: ["igor1309.dev"]
+            name: "igor1309dev",
+            targets: ["igor1309dev"]
         )
     ],
     dependencies: [
@@ -21,11 +21,15 @@ let package = Package(
         .package(
             url: "https://github.com/johnsundell/splashpublishplugin",
             from: "0.2.0"
-        )
+        ),
+        .package(
+            url: "https://github.com/pointfreeco/swift-snapshot-testing.git",
+            from: "1.9.0"
+        ),
     ],
     targets: [
         .executableTarget(
-            name: "igor1309.dev",
+            name: "igor1309dev",
             dependencies: [
                 .product(
                     name: "Publish",
@@ -35,6 +39,16 @@ let package = Package(
                     name: "SplashPublishPlugin",
                     package: "splashpublishplugin"
                 )
+            ]
+        ),
+        .testTarget(
+            name: "igor1309devTests",
+            dependencies: [
+                "igor1309dev",
+                .product(
+                    name: "SnapshotTesting",
+                    package: "swift-snapshot-testing"
+                ),
             ]
         )
     ]
